f6effa2afaafca6e5239f84379821263dfded428
Ian-Roanito <ianroanito027@gmail.com>
2026-04-18 23:26:46 -0300

    finalizado o projeto001

--- projetos-testes/projeto001/script-menu.js
+++ projetos-testes/projeto001/script-menu.js
@@ -2,8 +2,21 @@ const hamburguer = document.getElementById("hamburguer");
 const navList = document.querySelector(".nav-list");
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const header = document.querySelector("#header")
+
 hamburguer.addEventListener('click', () => {
 
     navList.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+})
+
+document.addEventListener("scroll", ()=> {
+
+    let scrollTop = window.scrollY;
+
+    if (scrollTop > 0) {
+        header.classList.add("rolar")
+    } else {
+        header.classList.remove("rolar")
+    }
 })
